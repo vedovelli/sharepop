2e49ed8f5f94ccae6c839c75e4a8150678c9d81a
Fábio Vedovelli <vedovelli@gmail.com>
2016-04-23 20:49:32 -0300

    Behaviour implemented with JS/jQuery. Implementing CSS now.

--- script/main.js
+++ script/main.js
@@ -5,34 +5,87 @@ var SharePop = {
         $output: $('#output'),
         $todos: $('#todos'),
         $todoList: $('#todoList'),
+        $newTodo: $('#newTodo'),
+        $todoContent: $('#todoContent'),
+        $todoForm: $('#todoForm'),
+        $addTodoButton: $('#addTodoButton'),
     },
 
     data: {
-        todos: [
-            { id: 1, title: 'Test 1' },
-            { id: 2, title: 'Test 2' },
-            { id: 3, title: 'Test 3' },
-            { id: 4, title: 'Test 4' },
-            { id: 5, title: 'Test 5' },
-        ],
+        todos: [],
     },
 
     methods: {
 
         setup: function () {
-            SharePop.dom.$todos.on('click', '.todo', function () {
-                window.console.log(this)
+            SharePop.dom.$todos.on('click', '.todo', SharePop.methods.toggleCompletedEventHandler);
+            SharePop.dom.$addTodoButton.on('click', SharePop.methods.newTodoEventHandler);
+            SharePop.dom.$todoForm.on('submit', SharePop.methods.newTodoEventHandler);
+
+            var savedList = window.localStorage.getItem('sharepop');
+            savedList = JSON.parse(savedList);
+            SharePop.data.todos = savedList.todos || [];
+        },
+
+        toggleCompletedEventHandler: function () {
+            var id = $(this).data('id');
+            var todo = SharePop.data.todos.filter(function (t) {
+                return t.id == id;
             });
+            if (todo.length == 1) {
+                SharePop.methods.toggleCompleted(todo[0])
+            }
+        },
+
+        toggleCompleted: function (todo) {
+            if (todo.completed == 1) {
+                todo.completed = 0;
+            } else {
+                todo.completed = 1;
+            }
+            SharePop.methods.assembleList(SharePop.data.todos);
+        },
+
+        newTodoEventHandler: function (ev) {
+            ev.preventDefault();
+            var next = SharePop.data.todos.length + 1;
+            var val = SharePop.dom.$todoContent.val()
+            if (val != '') {
+                var newTodo = {
+                    id: next,
+                    content: val,
+                    completed: 0
+                };
+                SharePop.methods.newTodo(newTodo);
+            } else {
+                SharePop.dom.$todoContent.val('').focus()
+            }
+        },
+
+        newTodo: function (data) {
+            SharePop.data.todos.unshift(data)
+            SharePop.methods.assembleList(SharePop.data.todos)
+            SharePop.dom.$todoContent.val('').focus()
         },
 
         assembleList: function (todos) {
             var markup = '';
             if (todos.length > 0) {
                 todos.forEach(function (todo) {
-                    markup += '<li class="todo" data-id="'+ todo.id +'">'+ todo.title +'</li>';
+                    var completed = todo.completed == 1 ? ' completed' : '';
+                    markup += '<li class="todo'+ completed +'" data-id="'+ todo.id +'">';
+                    markup += '<div>'+ todo.content +'</div>';
+                    markup += '</li>';
                 });
             }
             SharePop.dom.$todoList.html(markup);
+            SharePop.methods.saveList(todos)
+        },
+
+        saveList: function (todos) {
+            var obj = { todos: todos };
+            obj = JSON.stringify(obj);
+            window.localStorage.setItem('sharepop', obj);
         }
     },
 };
