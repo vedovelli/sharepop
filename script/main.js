bd6d539c55ae915f8326d8015e0a9256d4b4eb01
Fábio Vedovelli <vedovelli@gmail.com>
2016-04-23 10:24:42 -0300

    Some JS structure improvements

--- script/main.js
+++ script/main.js
@@ -1,31 +1,41 @@
 
-var app = {
+var SharePop = {
 
-    /**
-    * DOM
-    */
-    $output: $('#output'),
-    $todos: $('#todos'),
-    $todoList: $('#todoList'),
+    dom: {
+        $output: $('#output'),
+        $todos: $('#todos'),
+        $todoList: $('#todoList'),
+    },
 
-    /**
-    * Data
-    */
-    todos: [
-        { id: 1, title: 'Test 1' },
-        { id: 2, title: 'Test 2' },
-        { id: 3, title: 'Test 3' },
-        { id: 4, title: 'Test 4' },
-        { id: 5, title: 'Test 5' },
-    ],
+    data: {
+        todos: [
+            { id: 1, title: 'Test 1' },
+            { id: 2, title: 'Test 2' },
+            { id: 3, title: 'Test 3' },
+            { id: 4, title: 'Test 4' },
+            { id: 5, title: 'Test 5' },
+        ],
+    },
 
-    assembleList: function (todos) {
-        var markup = '';
-        if (todos.length > 0) {
-            todos.forEach(function (todo) {
-                markup += '<li data-id="'+ todo.id +'">'+ todo.title +'</li>';
+    methods: {
+
+        setup: function () {
+            SharePop.dom.$todos.on('click', '.todo', function () {
+                window.console.log(this)
             });
+        },
+
+        assembleList: function (todos) {
+            var markup = '';
+            if (todos.length > 0) {
+                todos.forEach(function (todo) {
+                    markup += '<li class="todo" data-id="'+ todo.id +'">'+ todo.title +'</li>';
+                });
+            }
+            SharePop.dom.$todoList.html(markup);
         }
-        this.$todoList.html(markup);
-    }
+    },
 };
+
+SharePop.methods.setup();
+SharePop.methods.assembleList(SharePop.data.todos);
